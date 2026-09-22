511d868a9f78e75ee4a8ce37c6f1d4896ff384b8
arifnazari862-droid <arifnazari862@gmail.com>
2026-09-22 17:00:51 +0700

    Add files via upload

---
+++ tipe_data_string_pert1.py
@@ -0,0 +1,22 @@
+# string sebaris
+string_1 = "hello python"
+
+# string multi-baris
+string_2 = """selamat
+Belajar
+Python"""
+
+# string sebaris
+string_3 = 'for the horde!'
+
+# string multi-baris
+string_4 = '''
+Sesuk
+Preiiii
+'''
+
+# string sebaris
+string_5 = '''\
+Sesuk
+Preiiii
+'''
